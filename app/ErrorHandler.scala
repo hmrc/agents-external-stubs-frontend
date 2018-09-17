4e5f7b60c8402f118ab984734b48f94088ba3c05
Artur Opala <opala.artur@gmail.com>
2018-09-17 19:31:07 +0100

    [APB-2196][AO] fix continue url

--- app/ErrorHandler.scala
+++ app/ErrorHandler.scala
@@ -37,8 +37,7 @@ class ErrorHandler @Inject()(
     implicit val r = Request(request, "")
     exception match {
       case _: NoActiveSession =>
-        toGGLogin(
-          if (isDevEnv) s"http://${request.host}${request.uri}" else s"http://localhost:$httpPort${request.uri}")
+        toGGLogin(if (isDevEnv) request.uri else s"http://localhost:$httpPort${request.uri}")
       case _: InsufficientEnrolments =>
         Forbidden(
           error_template(
