d62d12cb07bd355df27eab8dca07c333b6d807ff
Artur Opala <opala.artur@gmail.com>
2018-10-04 10:11:51 +0100

    [APB-3068][AO] help controller and help page skeleton

---
+++ app/uk/gov/hmrc/agentsexternalstubsfrontend/controllers/HelpController.scala
@@ -0,0 +1,29 @@
+package uk.gov.hmrc.agentsexternalstubsfrontend.controllers
+
+import javax.inject.{Inject, Singleton}
+import play.api.Configuration
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc._
+import uk.gov.hmrc.agentsexternalstubsfrontend.connectors.AgentsExternalStubsConnector
+import uk.gov.hmrc.agentsexternalstubsfrontend.services.{Features, ServicesDefinitionsService}
+import uk.gov.hmrc.agentsexternalstubsfrontend.views.html
+import uk.gov.hmrc.auth.core.AuthConnector
+import uk.gov.hmrc.auth.core.retrieve.Retrievals
+import uk.gov.hmrc.play.bootstrap.controller.FrontendController
+
+import scala.concurrent.Future
+
+@Singleton
+class HelpController @Inject()(
+  override val messagesApi: MessagesApi
+)(implicit val configuration: Configuration)
+    extends FrontendController with I18nSupport {
+
+  def showHelpPage(name: String): Action[AnyContent] = Action.async { implicit request =>
+    Future.successful(name).map {
+      case "agent-authorisation-api" => Ok(html.help.help_agent_authorisation_api())
+      case _                         => BadRequest("Help page not found.")
+    }
+  }
+
+}
