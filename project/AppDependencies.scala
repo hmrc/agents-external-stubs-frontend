9a4e5296b2b1d9b0a0994c9c16aa41ed8c60640b
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2023-03-09 16:22:31 +0000

    APB-6871 [CS] bump play-frontend-hmrc to 5.5

---
+++ project/AppDependencies.scala
@@ -0,0 +1,24 @@
+import play.sbt.PlayImport.ws
+import sbt._
+
+object AppDependencies {
+
+  lazy val compile = Seq(
+    ws,
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28" % "7.14.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "5.5.0-play-28",
+    "uk.gov.hmrc"       %% "agent-mtd-identifiers"      % "0.60.0-play-28",
+    "uk.gov.hmrc"       %% "play-partials"              % "8.4.0-play-28",
+    "uk.gov.hmrc"       %% "agent-kenshoo-monitoring"   % "4.8.0-play-28",
+    "com.typesafe.play" %% "play-json-joda"             % "2.9.2"
+  )
+
+  lazy val test =
+    Seq(
+      "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0"     % "test, it",
+      "org.scalatestplus"      %% "mockito-3-12"       % "3.2.10.0"  % "test, it",
+      "com.github.tomakehurst"  % "wiremock-jre8"      % "2.26.1"    % "test, it",
+      "com.vladsch.flexmark"    % "flexmark-all"       % "0.35.10"   % "test, it"
+    )
+
+}
