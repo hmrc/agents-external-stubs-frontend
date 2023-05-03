39e35529b620cca44935b7c13b23bf70c5fc3daf
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2023-05-03 10:47:23 +0100

    [CS] pr-bot

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,9 +3,11 @@ import sbt._
 
 object AppDependencies {
 
+  private val bootstrapVer = "7.15.0"
+
   lazy val compile = Seq(
     ws,
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28" % "7.15.0",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28" % bootstrapVer,
     "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "5.5.0-play-28",
     "uk.gov.hmrc"       %% "agent-mtd-identifiers"      % "1.2.0",
     "uk.gov.hmrc"       %% "play-partials"              % "8.4.0-play-28",
@@ -15,10 +17,11 @@ object AppDependencies {
 
   lazy val test =
     Seq(
-      "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0"     % "test, it",
-      "org.scalatestplus"      %% "mockito-3-12"       % "3.2.10.0"  % "test, it",
-      "com.github.tomakehurst"  % "wiremock-jre8"      % "2.26.1"    % "test, it",
-      "com.vladsch.flexmark"    % "flexmark-all"       % "0.35.10"   % "test, it"
+      "uk.gov.hmrc"            %% "bootstrap-test-play-28" % bootstrapVer % "test, it",
+      "org.scalatestplus.play" %% "scalatestplus-play"     % "5.1.0"      % "test, it",
+      "org.scalatestplus"      %% "mockito-3-12"           % "3.2.10.0"   % "test, it",
+      "com.github.tomakehurst"  % "wiremock-jre8"          % "2.26.1"     % "test, it",
+      "com.vladsch.flexmark"    % "flexmark-all"           % "0.35.10"    % "test, it"
     )
 
 }
