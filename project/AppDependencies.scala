d4ab7bdbae8b2548f6a9aff22907241c98183d3f
Lewis Hay <29063729+lewishay@users.noreply.github.com>
2025-07-03 16:30:51 +0100

    Add PATCH method (#92)
    * Add PATCH method
    
    * Address platops bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,14 +3,14 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVer = "9.11.0"
+  private val bootstrapVer = "9.13.0"
 
   lazy val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVer,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.0.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.6.0",
     "uk.gov.hmrc"       %% "agent-mtd-identifiers"      % "2.2.0",
-    "uk.gov.hmrc"       %% "play-partials-play-30"      % "10.0.0",
+    "uk.gov.hmrc"       %% "play-partials-play-30"      % "10.1.0",
     "org.playframework" %% "play-json-joda"             % "3.0.4"
   )
 
