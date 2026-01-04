3fc9abbbf5b64a55a6ee4d1db784fec771d25336
Ian Walton <51008396+ian-1@users.noreply.github.com>
2026-01-04 12:51:20 +0000

    APB-10289 - update dependency versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,12 +3,12 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVer = "10.4.0"
+  private val bootstrapVer = "10.5.0"
 
   lazy val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVer,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.20.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.25.0",
     "uk.gov.hmrc"       %% "agent-mtd-identifiers"      % "2.2.0",
     "uk.gov.hmrc"       %% "play-partials-play-30"      % "10.2.0",
     "org.playframework" %% "play-json-joda"             % "3.0.4"
