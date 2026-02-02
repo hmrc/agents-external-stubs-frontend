fbe46cc529f1ce4940c1cc77c62a4a9620f4d40b
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2026-02-02 11:54:38 +0000

    APB-10289: Changes userId to searchUserId in UserFiltersForm to avoid name clash

--- app/uk/gov/hmrc/agentsexternalstubsfrontend/controllers/UserController.scala
+++ app/uk/gov/hmrc/agentsexternalstubsfrontend/controllers/UserController.scala
@@ -513,7 +513,7 @@ class UserController @Inject() (
               for {
                 groups <- agentsExternalStubsConnector.getGroups
                 users <- agentsExternalStubsConnector.getUsers(
-                           userId = filters.userId.filter(_.nonEmpty),
+                           userId = filters.searchUserId.filter(_.nonEmpty),
                            groupId = filters.groupId.filter(_.nonEmpty),
                            principalEnrolmentService = filters.principalEnrolmentService.filter(_.nonEmpty),
                            limit = filters.limit
